19b537d89662d049f379d7bcbf1180fc502ead49
cd <dfjdklfj@>
2021-06-16 02:22:27 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser =require('body-parser');
+const {User} = require("./models/User");
+const config = require('./config/key')
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 const mongoose =require('mongoose')
-mongoose.connect('mongodb+srv://choing:as1234@cluster0.knwng.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
 }).then(() => console.log('MongDB Connected...'))
+. catch(err=>console.log(err))
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Worldtesteste!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/', (req, res) => res.send('hello world!~ '))
+
+app.post('/register', (req,res)=>{
+
+    const user = new User(req.body)
+    user.save((err, doc)=>{
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
 })
